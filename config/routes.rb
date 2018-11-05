a638fb720b1d8baf8d0b4a6ce5b245abdc539a82
riyonasan <legegend@gmail.com>
2018-11-05 12:18:16 +0900

    add devise and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'areas/index'
+  get 'genres/index'
+  root 'shops#index'
+  get 'shops/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
