7bae54615bd16d5787465479511eeebbea67066d
riyonasan <legegend@gmail.com>
2018-11-20 09:53:37 +0900

    add devise view

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   root 'shops#index'
   get 'shops/show'
 
-  resources :user
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'shops#index'
   get 'shops/show'
