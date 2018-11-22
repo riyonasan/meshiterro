0155e1013080ed25d080c51d82081345c9eb328d
灘　梨菜 <>
2018-11-22 10:33:24 +0900

    トップページにジャンル別おすすめリストを表示

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,7 @@
 class ShopsController < ApplicationController
   def index
     @shops = Shop.includes(:images).limit(3)
+    @square_img_url = Image.find_by(status: 1).image.to_s
     @shop_1 = Shop.find(1)
     @shop_2 = Shop.find(2)
     @shop_3 = Shop.find(3)
