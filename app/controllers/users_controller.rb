c334129b01178253fe9ae2e9b34bcceb7634dcbb
riyonasan <legegend@gmail.com>
2018-11-26 14:42:04 +0900

    平均評価を表示した。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def show
     @user = User.find(1)
     @user_like = @user.user_shops.where(like: 'true')
     @liked_shop = Shop.where(id: @user_like)
     @liked_genre = @liked_shop.map(&:genres).flatten
+    @rated_shop = Shop.where(id: @user.user_shops.where.not(rate: nil))
+    rates = @user.user_shops.where.not(rate: nil).average(:rate)
+    rates = rates.*10
+    @rates = rates.round
     gon.labels = []
     gon.data = []
   end
