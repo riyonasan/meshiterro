3e0f52629279e5a7a80c5bc5c43babd82d65dede
灘　梨菜 <>
2018-12-05 13:54:22 +0900

    fixed like function

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,6 @@
 class ShopsController < ApplicationController
+  before_action :like_find, only: [:show, :update]
+  after_action :like_update, only: :update
   def index
     @shops = Shop.includes(:images).limit(3)
     @square_img_url = Image.find_by(status: 1).image.to_s
@@ -9,29 +11,29 @@ class ShopsController < ApplicationController
   end
 
   def show
-    @shop = Shop.find(params[:id])
-
-    if user_signed_in?
-      @user_shop = UserShop.find_by(user_id: current_user.id, shop_id: @shop.id)
-      if @user_shop.nil?
-        @user_shop = UserShop.create(user_id: current_user.id, shop_id: @shop.id, like: 0)
-      end
+    if @user_shop.nil?
+      @user_shop = UserShop.create(user_id: current_user.id, shop_id: @shop.id, like: 0)
     end
   end
 
   def update
+    render json: @user_shop.like
+  end
+
+  private
+
+  def like_find
     @shop = Shop.find(params[:id])
     if user_signed_in?
       @user_shop = UserShop.find_by(user_id: current_user.id, shop_id: @shop.id)
     end
-    render json: @user_shop.like
-    # きもい
-
-    if @user_shop.like = false
-      @user_shop.update_attribute(like: true)
-    elsif @user_shop.like = true
-      @user_shop.update_attribute(like: false)
-    end
   end
 
+  def like_update
+    if @user_shop.like == false
+      @user_shop.update(like: true)
+    elsif @user_shop.like == true
+      @user_shop.update(like: false)
+    end
+  end
 end
--- app/controllers/user_shops_controller.rb
+++ app/controllers/user_shops_controller.rb
@@ -1,16 +1,2 @@
 class UserShopsController < ApplicationController
-  def update
-    # @user_shop = UserShop.find(params[:id])
-    # @shop = @user_shop.shop
-
-    # post_text = params[:data][:text]
-    # results = { :message => post_text }
-    # respond_to do |format|
-      # format.html
-      # format.json {render json: @user_shop}
-      # format.partial { render partial:'shops/user_shop'}
-    # end
-
-    # render 'update', formats: 'json', handlers: 'jbuilder'
-  end
 end
