2a724d3c028abebc04498ab31e13f596bc5a006a
灘　梨菜 <>
2019-01-09 13:04:45 +0900

    no message

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -3,11 +3,12 @@ class ShopsController < ApplicationController
   after_action :like_update, only: :update
   def index
     @shops = Shop.includes(:images).limit(3)
-    @square_img_url = Image.find_by(status: 1).image.to_s
+    @rank_shops = Shop.includes(:images).limit(3)
+
+    @square_img_url = Image.square.first.image.to_s
     @shop_1 = Shop.find(1)
     @shop_2 = Shop.find(2)
     @shop_3 = Shop.find(3)
-    # きもい
   end
 
   def show
