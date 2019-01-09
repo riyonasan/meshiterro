1b0c9791c329369b7b66212a95aa7ad2da3a2abe
灘　梨菜 <>
2019-01-09 11:53:24 +0900

    スコープを追加

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,11 +1,17 @@
 class GenresController < ApplicationController
   def ramen
-    genre_shops = GenreShop.where(genre_id: 1).map{ |a| a.shop_id }
-    @images = Image.where(shop_id: genre_shops, status: 1)
+    genre_shops = GenreShop.with_genres(1).map(&:shop_id)
+    @images = Image.where(shop_id: genre_shops).square
   end
 
   def sushi
-    genre_shops = GenreShop.where(genre_id: 2).map{ |a| a.shop_id }
-    @images = Image.where(shop_id: genre_shops, status: 1)
+    genre_shops = GenreShop.with_genres(2).map(&:shop_id)
+    @images = Image.where(shop_id: genre_shops).square
+  end
+
+  def teishoku
+  end
+
+  def izakaya
   end
 end
