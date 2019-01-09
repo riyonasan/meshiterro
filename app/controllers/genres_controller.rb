1d2107e0c249a32912a40ebac4cd04b4226c68ca
灘　梨菜 <>
2019-01-09 10:18:24 +0900

    部分テンプレートで各ジャンルごとのイメージ画像を出した

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,11 +1,11 @@
 class GenresController < ApplicationController
   def ramen
     genre_shops = GenreShop.where(genre_id: 1).map{ |a| a.shop_id }
-    @images = Image.where(shop_id: genre_shops, status: 1).map{ |b| b.image.to_s }
+    @images = Image.where(shop_id: genre_shops, status: 1)
   end
 
   def sushi
     genre_shops = GenreShop.where(genre_id: 2).map{ |a| a.shop_id }
-    @images = Image.where(shop_id: genre_shops, status: 1).map{ |b| b.image.to_s }
+    @images = Image.where(shop_id: genre_shops, status: 1)
   end
 end
