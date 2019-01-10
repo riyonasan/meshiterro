d4c6e2cc25fbbc4313556eea38849ba832ece575
灘　梨菜 <>
2019-01-10 11:31:23 +0900

    パンくずリストをDBから持ってくるように

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -5,6 +5,7 @@ class ShopsController < ApplicationController
     @shops = Shop.includes(:images).limit(3)
     @rank_shops = Shop.includes(:images).limit(3)
 
+    # 以下あとで直す
     @square_img_url = Image.square.first.image.to_s
     @shop_1 = Shop.find(1)
     @shop_2 = Shop.find(2)
@@ -12,9 +13,16 @@ class ShopsController < ApplicationController
   end
 
   def show
-    if @user_shop.nil?
-      @user_shop = UserShop.create(user_id: current_user.id, shop_id: @shop.id, like: 0)
+    # いいね機能
+    if user_signed_in?
+      @user_shop = UserShop.find_by(user_id: current_user.id, shop_id: @shop.id)
+      if @user_shop.nil?
+        @user_shop = UserShop.create(user_id: current_user.id, shop_id: @shop.id, like: 0)
+      end
     end
+
+    genre_shop =  GenreShop.where(shop_id: @shop.id).first
+    @genre = Genre.find(genre_shop.genre_id)
   end
 
   def update
@@ -25,9 +33,6 @@ class ShopsController < ApplicationController
 
   def like_find
     @shop = Shop.find(params[:id])
-    if user_signed_in?
-      @user_shop = UserShop.find_by(user_id: current_user.id, shop_id: @shop.id)
-    end
   end
 
   def like_update
