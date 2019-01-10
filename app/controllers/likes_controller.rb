cc46ccc79ceb49b3ba5917324a826d97495e5c95
灘　梨菜 <>
2019-01-10 18:23:01 +0900

    ajax無しでlikeテーブルの作成・削除を実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,19 +1,17 @@
 class LikesController < ApplicationController
   before_action :like_find, only: [:create, :destroy]
   def create
-    if like.nul?
-      like = Like.create(user_id: current_user.id, shop_id: @shop.id)
-    end
-
+    like = Like.create(user_id: current_user.id, shop_id: params[:shop_id])
   end
 
   def destroy
+    like = Like.find_by(user_id: current_user.id, shop_id: params[:shop_id])
+    like.destroy
   end
 
   private
 
   def like_find
-    @shop = Shop.find(params[:id])
-    like = Like.find_by(user_id: current_user.id, shop_id: @shop.id)
+    @shop = Shop.find(params[:shop_id])
   end
 end
