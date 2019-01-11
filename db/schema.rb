d38957bf12a97b06e735d29388a861dadca30bb8
riyonasan <legegend@gmail.com>
2019-01-11 12:38:15 +0900

    検索機能を除くヘッダーの部分テンプレート実装。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_01_10_054306) do
 
   create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
