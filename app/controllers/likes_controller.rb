d5f8db7eb291824b95f8c00f60472a42d55a1e7f
灘　梨菜 <>
2019-01-11 12:39:20 +0900

    いいねしたあとは評価済みと表示

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,4 @@ class LikesController < ApplicationController
     like = Like.create(user_id: current_user.id, shop_id: params[:shop_id])
     @shop = like.shop
   end
-
-  def destroy
-    like = Like.find_by(user_id: current_user.id, shop_id: params[:shop_id])
-    like.destroy
-  end
-
-  private
-
 end
