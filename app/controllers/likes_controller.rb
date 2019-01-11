5c5a5d44d62c6a3cf0a81cde82cd9e390face950
灘　梨菜 <>
2019-01-11 11:56:28 +0900

    likes/create.js.erbを描画出来るように

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,6 @@
 class LikesController < ApplicationController
   def create
-    like = Like.create(user_id: current_user.id, shop_id: params[:shop_id])
+    Like.create(user_id: current_user.id, shop_id: params[:shop_id])
   end
 
   def destroy
