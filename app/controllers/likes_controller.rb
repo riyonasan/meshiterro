3239780904bb3f1388368a0e9e4b8e4925d681ce
灘　梨菜 <>
2019-01-17 10:40:54 +0900

    マイページに遷移するといいね一覧が。その先に評価一覧に遷移できるように。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,12 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    user = User.find(current_user.id)
+    user_like = user.likes.map(&:shop_id)
+    @liked_shop = Shop.with_shops(user_like)
+  end
+
   def create
     like = Like.create(user_id: current_user.id, shop_id: params[:shop_id])
     @shop = like.shop
--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,11 +1,16 @@
 class RatesController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    user = User.find(current_user.id)
+    user_rate = user.rates.map(&:shop_id)
+    @rated_shop = Shop.with_shops(user_rate)
+  end
+
   def update
     shop = Shop.find(params[:shop_id])
     rate = Rate.find_by(user_id: current_user.id, shop_id: shop.id)
     rate.update(rate_params)
-    # logger.debug @rate.errors.inspect
     redirect_to shop_path(shop)
   end
 
