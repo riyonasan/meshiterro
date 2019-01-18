5939f7a7fa353691410389704e7abe1275da760e
灘　梨菜 <>
2019-01-18 10:29:34 +0900

    application_controllerのserchメソッドとヘッダーの部分テンプレートを削除。 ・users/showで検索窓を出すか迷っています。検索窓のありなしでヘッダーが二種類できてしまったので部分テンプレートは一旦消去しました。 ・ヘッダーの部分テンプレートをlayoutに書いてapplicationコントローラーで操作するというのが誤作動を頻発させていたのでapplicationコントローラーのsearchメソッドも削除しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :search
-  def search
-  @query = Shop.search(params[:q])
-  @shop_res = @query.result(distinct: true)
-  end
 end
